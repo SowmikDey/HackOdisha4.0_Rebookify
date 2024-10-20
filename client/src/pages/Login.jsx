ed626c3408289503fa09dbba1760eb447c401ac4
Sowmik Dey <154355301+SowmikDey@users.noreply.github.com>
2024-10-20 19:37:13 +0530

    Update Login.jsx

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,7 +1,7 @@
 import  { useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
-import './Login.css';
+import '../css/Login.css';
 
 export const Login = () => {
   const [credentials, setCredentials] = useState({
